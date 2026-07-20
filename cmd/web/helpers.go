8db135682d4e0afa1e5da30f9349b79babc487f5
Zetshin <151037933+Zetshin@users.noreply.github.com>
2026-07-20 13:38:16 +0700

    Add runtime error catching

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -1,29 +1,36 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net/http"
 )
 func (app *application) render(w http.ResponseWriter, r *http.Request, status int, page string, data templateData) {
-// Retrieve the appropriate template set from the cache based on the page
-// name (like 'home.tmpl'). If no entry exists in the cache with the
-// provided name, then create a new error and call the serverError() helper
-// method that we made earlier and return.
 ts, ok := app.templateCache[page]
 if !ok {
 err := fmt.Errorf("the template %s does not exist", page)
 app.serverError(w, r, err)
 return
 }
-// Write out the provided HTTP status code ('200 OK', '400 Bad Request' etc).
-w.WriteHeader(status)
-// Execute the template set and write the response body. Again, if there
-// is any error we call the serverError() helper.
-err := ts.ExecuteTemplate(w, "base", data)
+// Initialize a new buffer.
+buf := new(bytes.Buffer)
+// Write the template to the buffer, instead of straight to the
+// http.ResponseWriter. If there's an error, call our serverError() helper
+// and then return.
+err := ts.ExecuteTemplate(buf, "base", data)
 if err != nil {
 app.serverError(w, r, err)
+return
 }
+// If the template is written to the buffer without any errors, it's safe
+// to go ahead and write the HTTP status code to http.ResponseWriter.
+w.WriteHeader(status)
+// Write the contents of the buffer to the http.ResponseWriter. Note: this
+// is another time where we pass our http.ResponseWriter to a function that
+// takes an io.Writer.
+buf.WriteTo(w)
 }
+
 func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
 	var (
 		method = r.Method
